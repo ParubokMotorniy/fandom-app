f727c4bc115dbd5784858fb1be1f539815e4080b
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2025-05-11 22:17:42 +0300

    media

--- page_adding_service/page_adding_service.py
+++ page_adding_service/page_adding_service.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from pydantic import BaseModel
 from confluent_kafka import Producer
 import uuid
 import json
+import base64
 
 app = FastAPI()
 kafka_config = {
@@ -15,20 +16,31 @@ class PageData(BaseModel):
     html: str
 
 @app.post("/add_page")
-async def add_page(data: PageData):
+async def add_page(data: PageData, media: UploadFile = None):
     try:
         page_id = str(uuid.uuid4())
+        # TODO get root url from consul
         custom_uri = f"https://localhost/pages/{page_id}"
 
         payload = {
             "id": page_id,
-            "original_url": data.url,
             "html": data.html,
             "custom_uri": custom_uri
         }
-
+        # push without media to page-search
         producer.produce("page-search-topic", json.dumps(payload).encode("utf-8"))
 
+        if media:
+            media_content = await media.read()
+            media_type = media.content_type
+            media_filename = media.filename
+
+            encoded_media = base64.b64encode(media_content).decode("utf-8")
+            payload["media"] = {
+                "filename": media_filename,
+                "content_type": media_type,
+                "data": encoded_media  # Now it's a string, ready for JSON
+            }
         producer.produce("page-retrival-topic", json.dumps(payload).encode("utf-8"))
 
         producer.flush(timeout=5)
