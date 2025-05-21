f5157e0dabc304cac02b09cadc0babcdc61e0179
hellcaster <muryn.pn@ucu.edu.ua>
2025-05-21 17:53:21 +0300

    kafka to search_service

--- page_adding_service/app/routes/add_page.py
+++ page_adding_service/app/routes/add_page.py
@@ -1,7 +1,7 @@
 # app/routes/add_page.py
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from app.services.add_page_logic import forward_page_to_retrieval_service
+from app.services.add_page_logic import forward_page_to_retrieval_service, add_to_search_service
 from app.schemas.page import PageCreate
 
 router = APIRouter()
@@ -12,6 +12,10 @@ async def add_page(request: PageCreate):
     success = forward_page_to_retrieval_service(request)
     if not success:
         raise HTTPException(status_code=500, detail="Failed to add page")
+    
+    success = add_to_search_service(request)
+    if not success:
+        raise HTTPException(status_code=500, detail="Failed to add page to search service")
     
     print("Page added successfully")
     return {"message": "Page added successfully"}
--- page_adding_service/app/services/add_page_logic.py
+++ page_adding_service/app/services/add_page_logic.py
@@ -49,3 +49,35 @@ def forward_page_to_retrieval_service(page_data: PageCreate) -> bool:
     except Exception as e:
         print(f"Failed to send page to Kafka: {e}")
         return False
+
+def add_to_search_service(page_data: PageCreate) -> bool:
+    try:
+        print("Getting Kafka producer")
+        producer = get_kafka_producer()
+        
+        # Get the topic name from Consul config
+        kafka_config = None
+        while kafka_config is None:
+            kafka_config = ch.read_value_for_key("kafka-config")
+    
+        topic_name = kafka_config["search-topic-name"]
+        print(f"Topic name: {topic_name}")
+        
+        # Convert page data to JSON
+        page_json = json.dumps(page_data.dict())
+        
+        # Publish message to Kafka
+        print(f"Publishing message to Kafka Search: {page_json}")
+        producer.produce(
+            topic=topic_name,
+            value=page_json.encode('utf-8'),
+            callback=lambda err, msg: print(f"Message delivery failed: {err}") if err else print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+        )
+        
+        # Wait for any outstanding messages to be delivered
+        producer.flush()
+        return True
+        
+    except Exception as e:
+        print(f"Failed to send page to Kafka: {e}")
+        return False
